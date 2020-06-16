9fac5201b7732aad3806fafc64353cc46f717ce9
Joaquin Ramirez <joaquinramirez97@yahoo.com>
2020-06-16 13:13:18 -0500

    Added shooting component

---
+++ UnityShareTest/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get player's position and rotation
+            Transform playerTransform = GetComponent<Transform>();
+
+            // Create bullet on players position and shoot it where ever the player is facing
+            var b = Instantiate(bulletPrefab, playerTransform.position, playerTransform.rotation);
+            b.GetComponent<Rigidbody>().AddForce(b.transform.forward * bulletSpeed, ForceMode.Impulse);
+
+            Destroy(b, 2.0f);
+        }
+    }
+}
