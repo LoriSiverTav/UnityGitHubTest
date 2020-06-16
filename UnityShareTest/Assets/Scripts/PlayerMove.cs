bd349c6d0c3a4f5e2931f8d47830115177be9e11
LauriceThomas <49695257+LauriceThomas@users.noreply.github.com>
2020-06-16 13:51:16 -0500

    Startup
    Added plane, added player, added move script, added materials, added Sctips folder

---
+++ UnityShareTest/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,64 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController playerController;
+    public Rigidbody playerRB;
+    public float speed;
+    public float jumpForce;
+    public float gravity;
+    public float turnSmoothTime;
+    private float turnSmoothVelocity;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        playerRB = GetComponent<Rigidbody>();
+        speed = 8f;
+        jumpForce = 12;
+        gravity = 20f;
+        turnSmoothTime = 0.1f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        playerRB.velocity = new Vector3(h * speed, playerRB.velocity.y, v * speed);
+
+        if (Input.GetKey(KeyCode.Space) && CheckGround())
+        {
+            playerRB.velocity = new Vector3(playerRB.velocity.x, jumpForce, playerRB.velocity.z);
+        }
+    }
+
+    public bool CheckGround()
+    {
+        RaycastHit hit;
+        float distance = 1f;
+        Vector3 dir = new Vector3(0, -1);
+
+        if (Physics.Raycast(transform.position, dir, out hit, distance))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
